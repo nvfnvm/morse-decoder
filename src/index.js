a6ed3b01b3e6ad2334d184a040019e1138f8e2d7
Pavel <neverfearnevermore1@gmail.com>
2022-02-24 14:37:44 +0300

    feat: update morse decoder func

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    morseConversion = '';
+    result = '';
+
+    for(let i = 0; i <= expr.length - 1; i += 10) {
+        let char = expr.slice(i, i + 10);
+        morseConversion = '';
+
+        for(let j = char.length; j >= 0; j -= 2) {
+            let piece = char.slice(j - 2, j);
+            if(piece === "10") {
+                morseConversion="."+morseConversion;
+            }if(piece === "11") {
+                morseConversion ="-"+morseConversion;
+            }
+        }
+        
+        if (MORSE_TABLE[morseConversion]) {
+            result += MORSE_TABLE[morseConversion];
+        } else {
+            result += " ";
+        }
+       
+    }
+    return result;
 }
 
 module.exports = {
